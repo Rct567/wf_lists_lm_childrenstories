df423ee63c4f6bbaed41503fdbd305c2189e2961
Rick Zuidhoek <rct999@gmail.com>
2025-02-14 20:14:01 +0100

    fixed num_text_files_in_dir and num_lines_in_file for non existing file/dir

--- common_lib.py
+++ common_lib.py
@@ -384,11 +384,16 @@ class StoryTitles:
                 f.write(f"{title}\n")
 
 def num_text_files_in_dir(dir_path: str) -> int:
+    if not os.path.exists(dir_path):
+        return 0
     files_in_dir = [file for file in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, file))]
     text_files_in_dir = [file for file in files_in_dir if file.endswith(".txt")]
     return len(text_files_in_dir)
 
 def num_lines_in_file(file_path: str) -> int:
+    if not os.path.exists(file_path):
+        print("File '{}' does not exist.".format(file_path))
+        return 0
     with open(file_path, 'r', encoding="utf-8") as f:
         return sum(1 for _ in f)
 
